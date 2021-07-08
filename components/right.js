43eef4286a4d00104d4e61d5a369a7c94e45d9b4
aldyfiantur13 <68587584+aldyfiantur13@users.noreply.github.com>
2021-07-08 18:24:41 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ components/right.js
@@ -0,0 +1,16 @@
+const Right = () => (
+ <div id="right-conten">
+<p>Iklan Laundry</p>
+<a href='/iklan/laundry'>
+<img src="images/iklan.jpg" />
+</a>
+
+<hr />
+<p>Iklan Mie Sedap</p>
+<a href='/iklan/miesedap'>
+<img src='/images/miesedap.jpg' />
+
+</a>
+ </div>
+)
+export default Right;
